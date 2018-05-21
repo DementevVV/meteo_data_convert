362f290cd608d73bb35031c6175da6c4e790a645
DementevVV <dem_dvv@mail.ru>
2018-05-22 00:02:45 +0300

    filtering

--- main.rb
+++ main.rb
@@ -1,24 +1,51 @@
-RES_PATH = "ff/res.txt"
+require 'csv'
+
+RES_PATH = "ff/result.csv"
 DATA_PATH = "ff"
-File.open(RES_PATH, "a") do |f|
-  text=""
-  year = 10..99
-  month = 1..12
-  day = 1..31
-  year.each do |y|
-    month.each do |m|
-      day.each do |d|
-        Dir.glob("#{DATA_PATH}/#{y}/#{m}/#{d}.txt") do |txt|
-          line_count = 0 
-          File.open("#{txt}").each do  |line|
-            f << line        
-            file = "#{txt}"
+class Converter
+  File.open(RES_PATH, "a") do |f|
+    f << "Year,Month,Day,Hour,Minute,Second,Temp,Hum,Press\n"
+    # text=""
+    year = 10..99
+    month = 1..12
+    day = 1..31
+    year.each do |y|
+      month.each do |m|
+        day.each do |d|
+          Dir.glob("#{DATA_PATH}/#{y}/#{m}/#{d}.txt") do |txt|
+            line_count = 0
+            File.open("#{txt}").each do  |line|
+              f << line.split(/[\/,:, ]/).join(',')
+              file = "#{txt}"
+            end
           end
         end
       end
     end
   end
 end
-lines = File.open(RES_PATH).each_line.count
-puts "Total lines: #{lines}" 
-puts "Res in: #{RES_PATH}"
+
+class DataFilter
+  # CSV.foreach(RES_PATH) do |row|
+  #   row[6] = row[6].to_f ## Temperature to float
+  #   row[7] = row[7].to_i ## Hum to integer
+  #   row[8] = row[8].to_f ## Press to float
+  #   row.delete(row) if row[6] > 40
+  #   puts "Error" if row[6] > 40
+  # end
+  @csvfile = RES_PATH
+  table = CSV.table(@csvfile)
+
+  table.delete_if do |row|
+    row[7].to_i > 100 || row[6].to_f > 40.0
+  end
+
+  File.open('ff/filtering.csv', 'w') do |f|
+    f.write(table.to_csv)
+  end
+end
+
+# lines = File.open(RES_PATH).each_line.count
+# puts "Total lines: #{lines}"
+@data = Converter.new
+@filter = DataFilter.new
